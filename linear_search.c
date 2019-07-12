c6c7f0bed0e57ab97e5845207e053fdc474c24ed
Madhav-0312 <madhavvbajaj@gmail.com>
2019-07-12 15:59:16 +0530

    linear search

---
+++ linear_search.c
@@ -0,0 +1,21 @@
+
+
+#include<stdio.h>
+void main()
+{int size;
+scanf("%d",&size);
+int a[size];
+for(int i=0;i<size;i++)
+{scanf("%d",&a[i]);
+}
+int pos,n;
+scanf("%d",&n);
+for(int i=0;i<size;i++)
+{if(a[i]==n)
+{pos=i;
+break;
+}
+}
+printf("%d",pos);
+}
+
