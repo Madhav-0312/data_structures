96a125378f13fb3a4b7fe7ae0f899d1b6d17a66b
Madhav-0312 <madhavvbajaj@gmail.com>
2019-07-19 12:16:10 +0530

    Ptr print

--- print.c
+++ print.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
 int main(void)
-{int arr[]={1,2,3,4};
-int *ptr=arr;
-int n=sizeof(arr)/sizeof(int);
-for(int i=0;i<n;i++)
-printf("%d",*(ptr+i));
+{int arr[]={1,2,3,4,'\0'};
+int *ptr;
+ptr=arr;
+while(*ptr!='\0')
+{
+printf("%d",*ptr);
+ptr++;
+}
 return 0;
 }
 
